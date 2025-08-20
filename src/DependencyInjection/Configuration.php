8fc64ec6229fa530b8da3e61ecaf002c684571d8
Yurii Panaiotov <y.panaiotov@foodtech.team>
2025-08-20 16:11:39 +0300

    fix phpstan

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace HealthCheck\DependencyInjection;
 
+use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
 
@@ -12,24 +13,26 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder(): TreeBuilder
     {
         $treeBuilder = new TreeBuilder('health_check');
+        
+        /** @var ArrayNodeDefinition $rootNode */
         $rootNode = $treeBuilder->getRootNode();
+        
+        // @phpstan-ignore-next-line
         $rootNode
             ->children()
-            ->arrayNode('apps')
-            ->example(['web', 'command'])
-            ->useAttributeAsKey('name')
-            ->arrayPrototype()
-            ->children()
-            ->arrayNode('checkers')
-            ->scalarPrototype()
-            ->end()
-            ->end()
-            ->end()
-            ->end()
-            ->end()
-            ->end()
-            ->end()
-        ;
+                ->arrayNode('apps')
+                    ->example(['web', 'command'])
+                    ->useAttributeAsKey('name')
+                    ->arrayPrototype()
+                        ->children()
+                            ->arrayNode('checkers')
+                                ->scalarPrototype()
+                                ->end()
+                            ->end()
+                        ->end()
+                    ->end()
+                ->end()
+            ->end();
 
         return $treeBuilder;
     }
